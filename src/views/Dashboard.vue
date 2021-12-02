d1eadd90585906ec5bb586bd182cc2ba64643336
Raj Bhatta <raj@sstwireless.com>
2021-12-02 14:06:53 -0800

    Table is added inside dashboard for listing sensors

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
+  <!-- start of container setup -->
   <div class="container mx-auto">
-    <!-- start of container setup -->
     <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-18 md:pb-5">
       <div class="flex flex-wrap">
         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
@@ -23,7 +23,9 @@
                 </div>
               </div>
               <div class="flex-1 text-right md:text-center">
-                <h5 class="font-bold uppercase text-gray-600">Total Sensors Record Counts</h5>
+                <h5 class="font-bold uppercase text-gray-600">
+                  Total Sensors Record Counts
+                </h5>
                 <h3 class="font-bold text-3xl">
                   15
                   <span class="text-green-500"
@@ -87,7 +89,9 @@
                 </div>
               </div>
               <div class="flex-1 text-right md:text-center">
-                <h5 class="font-bold uppercase text-gray-600">Working Sensors</h5>
+                <h5 class="font-bold uppercase text-gray-600">
+                  Working Sensors
+                </h5>
                 <h3 class="font-bold text-3xl">
                   2
                   <span class="text-yellow-600"
@@ -103,6 +107,85 @@
     </div>
   </div>
   <!-- end of container setup -->
+
+  <!-- start of table -->
+  <div class="container grid px-6 mx-auto">
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+      Sensor Details
+    </h2>
+
+    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+      <div class="w-full overflow-x-auto">
+        <table class="w-full whitespace-no-wrap">
+          <thead>
+            <tr
+              class="
+                text-xs
+                font-semibold
+                tracking-wide
+                text-left text-gray-500
+                uppercase
+                border-b
+                dark:border-gray-700
+                bg-gray-50
+                dark:bg-gray-800
+              "
+            >
+              <th class="px-4 py-3">Sensor ID</th>
+              <th class="px-4 py-3">Sensor Name</th>
+              <th class="px-4 py-3">Temperature (°C)</th>
+              <th class="px-4 py-3">Status</th>
+              <th class="px-4 py-3">Created At</th>
+            </tr>
+          </thead>
+          <tbody
+            class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+          >
+            <tr class="text-gray-700 dark:text-gray-400">
+              <td class="px-4 py-3">
+                <div class="flex items-center text-sm">
+                  <div>
+                    <p class="font-semibold">
+                      <router-link :to="'/sensors/' + 1"> 0xACAC </router-link>
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td class="px-4 py-3">
+                <div class="flex items-center text-sm">Test Sensor Name</div>
+              </td>
+              <td class="px-4 py-3">
+                <div class="flex items-center text-sm">15</div>
+              </td>
+              <td class="px-4 py-3">
+                <div class="flex items-center text-sm">
+                  <span
+                    class="
+                      px-2
+                      py-1
+                      font-semibold
+                      leading-tight
+                      text-green-700
+                      bg-green-100
+                      rounded-full
+                      dark:text-green-100
+                    "
+                  >
+                    Approved
+                  </span>
+                </div>
+              </td>
+
+              <td class="px-4 py-3">
+                <div class="flex items-center text-sm">6/10/2020</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <!-- end of table -->
 </template>
 
 <script>
