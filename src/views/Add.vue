09276a25a948bbe0d65ff9da16488660363d0170
Raj Bhatta <raj@sstwireless.com>
2021-12-02 15:57:01 -0800

    Add sensor form is added

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,9 +1,75 @@
 <template>
-  <div class="container grid px-6 mx-auto grid">
-      <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-       <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-              Add sensor
-        </h4>
+  <div class="container mx-auto grid">
+    <h4 class="mb-4 text-lg text-purple-800">Please Add Sensor</h4>
+
+    <div class="min-h-scree flex item-center justify-center">
+      <!--start of form -->
+      <div class="bg-white p-8 rounded shadow-2xl w-1/2">
+        <form class="space-y-10">
+          <div class="mb-3">
+            <label for="hexId" class="mr-2">HexId</label>
+            <input
+              type="text"
+              class="
+                w-full
+                border-2 border-gray-400
+                p-3
+                rounded
+                outline-none
+                focus:border-blue-500
+              "
+              placeholder="Please enter sensor hex id.."
+            />
+          </div>
+          <div class="mb-3">
+            <label for="hexId" class="mr-2">Name</label>
+            <input
+              type="text"
+              class="
+                w-full
+                border-2 border-gray-400
+                p-3
+                rounded
+                outline-none
+                focus:border-blue-500
+              "
+              placeholder="Please enter sensor name.."
+            />
+          </div>
+          <div class="mb-3">
+            <label for="hexId" class="mr-2">Location</label>
+            <input
+              type="text"
+              class="
+                w-full
+                border-2 border-gray-400
+                p-3
+                rounded
+                outline-none
+                focus:border-blue-500
+              "
+              placeholder="Please enter sensor location.."
+            />
+          </div>
+          <div class="mb-3">
+            <label for="hexId" class="mr-2">Temperature</label>
+            <input
+              type="text"
+              class="
+                w-full
+                border-2 border-gray-400
+                p-3
+                rounded
+                outline-none
+                focus:border-blue-500
+              "
+              placeholder="Please enter sensor temperature.."
+            />
+          </div>
+          <button class="block w-full bg-green-400 p-4 rounded">Submit</button>
+        </form>
       </div>
+      <!-- end of form -->
+    </div>
   </div>
 </template>
