4d4579ff7a8cffc522c7da065f662d980d5240d4
Raj Bhatta <bhattaraj21@gmail.com>
2021-12-03 02:28:20 -0800

    Task 2: Amplify push -y is done and snapshots are added

--- amplify/#current-cloud-backend/function/sensorlambdav3/src/index.py
+++ amplify/#current-cloud-backend/function/sensorlambdav3/src/index.py
@@ -1,6 +1,11 @@
+from modal.sensor import Sensor
+from sensorservice import SensorService
+from dbhandler import DbHandler
+
 import awsgi
 import json
 import logging
+from os import error
 import os
 
 from flask_cors import CORS
@@ -12,32 +17,97 @@ BASE_ROUTE ="/sensorsv3"
 app = Flask(__name__)
 CORS(app)
 
+logger = logging.getLogger()
+
 @app.route(BASE_ROUTE, methods=['POST'])
 def create_customer():
-    request_json = request.get_json()
-    hex_id = request_json.get('hexid')
-    name = request_json.get('name')
-    temperature = request_json.get('temperature')
-    location = request_json.get('location')
-   
-    return jsonify(message={
-        hex_id,
-        name,
-        temperature
-    })
+    try:
+        """
+            PRODUCTION REQUEST VALUE
+        """
+        request_json = request.get_json()
+        request_hex_id = request_json.get('hexid')
+        request_name = request_json.get('name')
+        request_temperature = request_json.get('temperature')
+        request_location = request_json.get('location')
+
+        """
+            LOCAL TESTING
+        """
+        #request_hex_id = '0x123'
+        #request_name = 'test name'
+        #request_temperature = 15
+        #request_location = 'Surrey'
+    
+        db_session = provide_database_session()
+        sensor_service = SensorService(db_session)
+        sensor_service.save_sensor(Sensor(name=request_name,hexid=request_hex_id,temperature=request_temperature, location =request_location))
+    
+        return jsonify(message='OK')
+    except error as e:
+         logger.critical(e)
+         return jsonify(message='FAILED')
+    
 
 @app.route(BASE_ROUTE, methods=['GET'])
 def list_customer():
-    return jsonify(
-        hexid='0x123',
-        name='Test Sensor',
-        location='Surrey',
-        temperature= 15
-    )
+    db_session = provide_database_session();
 
+    # todo make list to json
+    sensor_list =[]
+    sensor_service = SensorService(db_session)
+    sensors = sensor_service.get_sensor()
+    for sensor in sensors:
+        sensor_list.append(sensor.get_json)
+    return sensor_list
 
 def handler(event, context):
-  print('received event:')
-  print(event)
+  logger.info('received event:')
+  logger.info(event)
   return awsgi.response(app,event,context)
 
+def provide_database_session():
+    
+    """ 
+        PRODUCTION
+    """
+    dbusername = validate_db_username(os.environ.get("DB_USERNAME"))
+    dbpassword = validate_db_password(os.environ.get("DB_PASSWORD"))
+    dbhost = validate_db_host(os.environ.get("DB_HOST"))
+    dbname = validate_db_name(os.environ.get("DB_NAME"))
+
+    """
+        LOCAL TESTING
+    """
+    #dbusername = validate_db_username('postgres')
+    #dbpassword = validate_db_password('<Password>')
+    #dbhost = validate_db_host('<I have terminated RDS>')
+    #dbname = validate_db_name('postgres')
+
+    dbhandler = DbHandler(logger,dbusername, dbpassword, dbhost , dbname)
+    return dbhandler.get_orm_db_session()
+
+
+def validate_db_username(username):
+    if username != None:
+        return username
+    else:
+        logger.critical("UNABLE TO GET DB USERNAME FROM ENVIRONMENT CONFIG")
+
+def validate_db_password(password):
+    if password != None:
+        return password
+    else:
+        logger.critical("UNABLE TO GET DB PASSWORD FROM ENVIRONMENT CONFIG")
+
+def validate_db_host(host):
+    if host != None:
+        return host
+    else:
+        logger.critical("UNABLE TO GET DB HOSTNAME FROM ENVIRONMENT CONFIG")
+
+def validate_db_name(name):
+    if name != None:
+        return name
+    else:
+        logger.critical("UNABLE TO GET DB NAME FROM ENVIRONMENT CONFIG")
